26e63d99fff3e66ccac157ebd7d0c927520961ad
Aaren Cooper <popstarfreas2@gmail.com>
2017-06-21 13:45:52 +0100

    Add new damage controller

---
+++ PvPController/Controllers/DamageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Terraria;
+
+/// <summary>
+/// Deals with controlling damage received from players
+/// </summary>
+namespace PvPController.Controllers
+{
+    public static class DamageController
+    {
+        public delegate int DamageHandler(Player attacker, Player victim, Item weapon, int damage);
+        public static List<DamageHandler> Controllers = new List<DamageHandler>();
+
+        public static int DecideDamage(Player attacker, Player victim, Item weapon, int damage)
+        {
+            foreach (var controller in Controllers)
+            {
+                damage = controller(attacker, victim, weapon, damage);
+            }
+
+            return damage;
+        }
+    }
+}
