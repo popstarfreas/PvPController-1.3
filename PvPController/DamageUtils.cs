02bc6379dcd78b8360ee8b904e59f66f82bf9f21
Aaren Cooper <popstarfreas2@gmail.com>
2018-06-03 12:46:16 +0100

    Add beetle, solar, and shadow dodge reduction. Paladin's Shield is 1/2 done/

--- PvPController/DamageUtils.cs
+++ PvPController/DamageUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Terraria;
+using System.Linq;
 
 namespace PvPController
 {
@@ -16,8 +17,30 @@ namespace PvPController
         {
             float defenseMultiplier = Main.expertMode ? 0.75f : 0.5f;
             double outDamage = internalDamage - (player.statDefense * defenseMultiplier);
+            if (player.setSolar && player.solarShields > 0)
+            {
+                outDamage *= 0.7;
+            } else if (player.beetleDefense && player.beetleOrbs > 0)
+            {
+                outDamage *= (1.0 - (0.15 * player.beetleOrbs));
+            }
 
-            if (outDamage < 1) outDamage = 1;
+            if (player.defendedByPaladin)
+            {
+                var buffedFrom = Main.player.Where(p => p != null && p.team == player.team && p.team != 0 && p.Distance(player.Center) < 800.0 && p.hasPaladinShield && !p.dead && !p.immune).FirstOrDefault();
+                if (buffedFrom != null)
+                {
+                    outDamage *= 0.75;
+                    // TODO: Implement damaging other player
+                }
+            }
+
+            if (outDamage < 1) outDamage = 1.0;
+
+            if (player.shadowDodge)
+            {
+                return 0.0;
+            }
 
             return outDamage * (1 - player.endurance);
         }
